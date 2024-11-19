6d60279b098e6b965c34a054f32b6fb8785e0de7
Farhan Ali Reza <farhanalireza50@gmail.com>
2024-11-19 14:28:27 +0530

    minor009

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
+const aside = document.getElementById("music-section");
+// aside.style.translate = 0;
+
+
 // Create the background audio
-const audio = new Audio('music/Game_Music.mp3'); // Replace with your music file path
+const audio1 = new Audio('./music/Game_Music.mp3'); // Replace with your music file path
+const audio2 = new Audio('./music/vintage-movie-piano-252205.mp3'); // Replace with your music file path
 audio.loop = true;
 audio.volume = 0.5;
 
-function playAudio() {
-    audio.play();
+function playAudio1() {
+    audio1.play();
+    audio2.pause();
+    aside.classList.toggle("open");
+    console.log(aside.classList); // Check if "open" is toggled
+}
+function playAudio2() {
+    audio2.play();
+    audio1.pause();
+    aside.classList.toggle("open");
+    console.log(aside.classList); // Check if "open" is toggled
 }
 
 function pauseAudio() {
-    audio.pause();
+    audio1.pause();
+    audio2.pause();
+    // aside.classList.toggle("open");
+    // console.log(aside.classList); // Check if "open" is toggled
 }
 
 window.addEventListener('load', playAudio);
@@ -31,20 +48,21 @@ const buttonSFX = new Audio("music/SFX/buttonSFX.mp3"); // Replace with your sou
 });
 
 playButton.addEventListener("click", () => {
-    window.open('./Games/click-the-target/index.html');
+    setTimeout(() => {
+        window.open('./Games/index.html');
+    }, 500)
 } );
 
-playButton.addEventListener("click", () => {
-    window.open('./Games/click-the-target/index.html');
-} );
+// playButton.addEventListener("click", () => {
+//     window.open('./Games/click-the-target/index.html');
+// } );
 
-playButton.addEventListener("click", () => {
-    window.open('./Games/click-the-target/index.html');
-} );
-
-playButton.addEventListener("click", () => {
-    window.open('./Games/click-the-target/index.html');
-} );
+// playButton.addEventListener("click", () => {
+//     window.open('./Games/click-the-target/index.html');
+// } );
+// playButton.addEventListener("click", () => {
+//     window.open('./Games/click-the-target/index.html');
+// } );
 
 
 
